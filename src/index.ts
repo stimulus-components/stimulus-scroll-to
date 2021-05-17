3331247aa61aad5a90fd32895644ac540c8da2af
Guillaume Briday <guillaumebriday@gmail.com>
2021-05-17 19:35:00 +0200

    Moving from ES6 to TypeScript and Snowpack to Vite

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,60 @@
 import { Controller } from 'stimulus'
 
+interface Option {
+  offset?: number
+  behavior?: string
+}
+
 export default class extends Controller {
+  offset: number
+  behavior: string
+
+  // @ts-ignore
+  element: HTMLAnchorElement
+  offsetValue: number
+  behaviorValue: string
+
   static values = {
     offset: Number,
     behavior: String
   }
 
-  initialize () {
+  initialize (): void {
     this.scroll = this.scroll.bind(this)
   }
 
-  connect () {
+  connect (): void {
     this.element.addEventListener('click', this.scroll)
 
     this.offset = this.offsetValue || this.defaultOptions.offset || 10
     this.behavior = this.behaviorValue || this.defaultOptions.behavior || 'smooth'
   }
 
-  disconnect () {
+  disconnect (): void {
     this.element.removeEventListener('click', this.scroll)
   }
 
-  scroll (event) {
+  scroll (event: Event): void {
     event.preventDefault()
 
-    const id = this.element.hash.replace(/^#/, '')
-    const target = document.getElementById(id)
+    const id: string = this.element.hash.replace(/^#/, '')
+    const target: HTMLElement = document.getElementById(id)
 
     if (!target) {
       console.warn(`[stimulus-scroll-to] The element with the id: "${id}" does not exist on the page.`)
       return
     }
 
-    const elementPosition = target.getBoundingClientRect().top + window.pageYOffset
-    const offsetPosition = elementPosition - this.offset
+    const elementPosition: number = target.getBoundingClientRect().top + window.pageYOffset
+    const offsetPosition: number = elementPosition - this.offset
 
-    window.scrollTo({
+    window.scrollTo(<ScrollToOptions>{
       top: offsetPosition,
       behavior: this.behavior
     })
   }
 
-  get defaultOptions () {
+  get defaultOptions (): Option {
     return {}
   }
 }
